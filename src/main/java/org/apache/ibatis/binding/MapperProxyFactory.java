986da2af696d1a829926452186d7007336b20f1f
wjx_Gin <1162547771@qq.com>
2023-07-23 22:52:50 +0800

    mybatis Binding模块

--- src/main/java/org/apache/ibatis/binding/MapperProxyFactory.java
+++ src/main/java/org/apache/ibatis/binding/MapperProxyFactory.java
@@ -27,7 +27,13 @@ import org.apache.ibatis.session.SqlSession;
  */
 public class MapperProxyFactory<T> {
 
+  /**
+   * Mapper 接口
+   */
   private final Class<T> mapperInterface;
+  /**
+   * 方法与 MapperMethod 的映射
+   */
   private final Map<Method, MapperMethod> methodCache = new ConcurrentHashMap<>();
 
   public MapperProxyFactory(Class<T> mapperInterface) {
